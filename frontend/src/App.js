a25c45825df7391e433d23e783ad64646a9e2101
Abhishek Jain <abhijain@Abhisheks-MacBook-Pro.local>
2021-05-30 16:38:11 +0530

    Added the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Video from "./components/Video/Video";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Home}></Route>
+        <Route path="/:url" component={Video}></Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
